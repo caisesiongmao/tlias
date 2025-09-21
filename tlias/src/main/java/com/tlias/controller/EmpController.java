ea7f4d180c75576489fbc35d4b4193b1537322b1
shiyu41 <shiyu41@meituan.com>
2025-09-21 18:13:47 +0800

    OSS上传文件，待优化成@Value形式存储字符串

--- tlias/src/main/java/com/tlias/controller/EmpController.java
+++ tlias/src/main/java/com/tlias/controller/EmpController.java
@@ -5,10 +5,12 @@ import com.tlias.req.newEmpReq;
 import com.tlias.req.queryEmpPagedReq;
 import com.tlias.resp.QueryEmpResp;
 import com.tlias.service.EmpService;
+import com.tlias.utils.aliyunUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -20,8 +22,11 @@ public class EmpController {
     @Autowired
     private EmpService empService;
 
+    @Autowired
+    private aliyunUtils aliyunUtils;
+
     @GetMapping
-    public Result queryEmpPaged(queryEmpPagedReq req) {
+    public Result queryEmpPaged(@RequestBody queryEmpPagedReq req) {
         log.info("queryEmp, pageNo:{}, pageSize:{}, name:{}, gender:{}, begin:{}, end:{}", req.getPageNo(), req.getPageSize(), req.getName(),
                 req.getGender(), req.getBegin(), req.getEnd());
         QueryEmpResp resp = empService.queryEmp(req);
@@ -36,4 +41,17 @@ public class EmpController {
         return Result.success(null);
     }
 
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) {
+        log.info("avatar upload, fileName:{}", file.getOriginalFilename());
+        try {
+            byte[] bytes = file.getBytes();
+            String url = aliyunUtils.uploadFile(file.getOriginalFilename(), bytes);
+            log.info("upload file, url:{}", url);
+            return Result.success(url);
+        } catch (Exception e) {
+            log.error("upload, error:{}", e.getMessage());
+            return Result.error("上传失败");
+        }
+    }
 }
---
+++ tlias/src/main/java/com/tlias/utils/aliyunUtils.java
@@ -0,0 +1,74 @@
+package com.tlias.utils;
+
+import com.aliyun.oss.*;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.common.comm.SignVersion;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class aliyunUtils {
+
+    // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+    private String endpoint = "https://oss-cn-beijing.aliyuncs.com";
+
+    // 填写Bucket名称，例如examplebucket。
+    private String bucketName = "shiyu-tlias";
+
+    private String region = "cn-beijing";
+
+    public String uploadFile(String originFileName, byte[] content) throws Exception {
+
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+
+        String dir = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        String random = UUID.randomUUID().toString();
+        String objectName = dir + "/" + random + originFileName.substring(originFileName.lastIndexOf("."));
+
+        // 创建OSSClient实例。
+        // 当OSSClient实例不再使用时，调用shutdown方法以释放资源。
+        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
+        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
+        OSS ossClient = OSSClientBuilder.create()
+                .endpoint(endpoint)
+                .credentialsProvider(credentialsProvider)
+                .clientConfiguration(clientBuilderConfiguration)
+                .region(region)
+                .build();
+
+        try {
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content));
+            // 创建PutObject请求。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+        return endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + objectName;
+    }
+}
